c408d6df1a50a3e45064b8e8be29fed920f4f178
tanveersran <tanveersran07@gmail.com>
2023-06-13 13:53:47 -0400

    update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -11,7 +11,7 @@ export default function Skills({skills}: Props) {
   return (
     <motion.div className='h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
         <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">&nbsp;Skills</h3>
-        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for current proficiency</h3>
+        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover to view skill title</h3>
 
         <div className='mt-20 sm:mt-0 grid grid-cols-3 sm:grid-cols-4 gap-5'>
           {skills?.slice(0, skills.length / 2).map(skill => (
