df1aa9acc4925bba893c22be23423808966702d7
tanveersran <tanveersran07@gmail.com>
2023-06-13 22:08:47 -0400

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -16,7 +16,8 @@ export default function Projects({projects}: Props) {
       className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
     >
       <h3 className="absolute top-24 uppercase tracking-[10px] sm:tracking-[20px] text-gray-500 text-2xl">
-        Projects
+        Projects <br/>
+        Press < > to scroll
       </h3>
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 mt-5 sm:mt-0">
